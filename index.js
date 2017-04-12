8108f44cd83cd009f1b79e7effe78547f3226244
paulpriester <paulpriester507@gmail.com>
2017-04-11 20:54:35 -0400

    Init files

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(){
+	//console.log("let's add");
+	var math= (7+5);
+	console.log(math);
+}
+
+function subtract(){
+	//console.log("let's subtract");
+	console.log(9-3);
+}
+
+function multiply(){
+	//console.log("let's multiply");
+	console.log(4*5);
+}
+
+function divide(){
+	//console.log("let's divide");
+	console.log(30/5);
+}
